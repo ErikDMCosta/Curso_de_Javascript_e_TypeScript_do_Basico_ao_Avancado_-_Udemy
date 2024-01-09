02459ed51e647b668d21dd89b9ec2a9e01edcaf8
ErikDMCosta <53874072+ErikDMCosta@users.noreply.github.com>
2024-01-08 21:48:35 -0300

    Enviando a prática de Express Webpack

---
+++ (AAA)-PraticasCurso/(098)-Aula98/(012)-AulaNode12/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path'); // CommonJS
+
+module.exports = {
+  mode: 'production',
+  entry: './frontend/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public', 'assets', 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      exclude: /node_modules/,
+      test: /\.js$/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env']
+        }
+      }
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
+  },
+  devtool: 'source-map',
+  resolve: {
+    alias: {
+      Assets: path.resolve(__dirname, 'frontend/assets/'),
+      Modules: path.resolve(__dirname, 'frontend/modules/')
+    }
+  }
+};
