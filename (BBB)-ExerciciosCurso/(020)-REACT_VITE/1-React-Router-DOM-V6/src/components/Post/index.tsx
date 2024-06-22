066b17c0f13d044d05e6460cfd152ac29e95d5f3
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2024-06-22 15:06:58 -0300

    Add project ´React Router DOM V6´ and ´React 18 Vite StoryBook Jest Styled Components´

---
+++ (BBB)-ExerciciosCurso/(020)-REACT_VITE/1-React-Router-DOM-V6/src/components/Post/index.tsx
@@ -0,0 +1,15 @@
+import './style.css';
+
+export const Post = () => {
+  return (
+    <div>
+      <h1>Post</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique nisi
+        sed porro optio cumque numquam in laboriosam totam itaque neque
+        provident, odio, aperiam tempore veniam expedita quae velit modi
+        ratione.
+      </p>
+    </div>
+  );
+};
