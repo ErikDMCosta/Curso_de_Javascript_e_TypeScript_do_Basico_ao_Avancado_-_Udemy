b66af19fc9a3a37bb2a6250d631562ac9499adc2
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2024-08-10 16:26:59 -0300

    Enviando Práticas do Webpack #1

---
+++ (BBB)-ExerciciosCurso/(022)-Iniciando_TS/src/A0018-WebPack/index.ts
@@ -0,0 +1,4 @@
+// console.log('Olá mundo!');
+// import './mod';
+import funcao from './mod';
+funcao();
---
+++ (BBB)-ExerciciosCurso/(022)-Iniciando_TS/src/A0018-WebPack/mod.ts
@@ -0,0 +1,4 @@
+// console.log('Sou o módulo.')
+export default (): void => {
+  console.log('Sou o módulo.')
+}
