89ed9f5211314e4ab4cda401b17dcca80d2d4fe6
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2024-08-14 19:31:29 -0300

    Enviando Práticas do Exercício - Validando um formulário

---
+++ (BBB)-ExerciciosCurso/(022)-Iniciando_TS/src/A0019-Exercicio/form-control.ts
@@ -0,0 +1,66 @@
+import isEmail from 'validator/lib/isEmail';
+
+const SHOW_ERROR_MESSAGES = 'show-error-messages';
+
+const form = document.querySelector('.form') as HTMLFormElement;
+const username = document.querySelector('.username') as HTMLInputElement;
+const email = document.querySelector('.email') as HTMLInputElement;
+const password = document.querySelector('.password') as HTMLInputElement;
+const password2 = document.querySelector('.password2') as HTMLInputElement;
+
+form.addEventListener('submit', function (event: Event) {
+  event.preventDefault();
+  hideErrorMessages(this);
+  checkForEmptyFields(username, email, password, password2);
+  checkEmail(email);
+  checkEqualPasswords(password, password2);
+  // if(shouldSendForm(this)) form.submit();
+  if (shouldSendForm(this)) console.log('FORMULÁRIO ENVIADO');
+});
+
+function checkForEmptyFields(...inputs: HTMLInputElement[]): void {
+  inputs.forEach((input) => {
+    if (!input.value) {
+      showErrorMessage(input, 'Esse campo não pode ficar vazio');
+    }
+  });
+}
+
+function checkEqualPasswords(
+  password: HTMLInputElement,
+  password2: HTMLInputElement,
+): void {
+  if (password.value !== password2.value) {
+    showErrorMessage(password, 'Senhas não batem');
+    showErrorMessage(password2, 'Senhas não batem');
+  }
+}
+
+function checkEmail(input: HTMLInputElement): void {
+  if (!isEmail(input.value)) {  // Corrigido
+    showErrorMessage(input, 'Email inválido');
+  }
+}
+
+function hideErrorMessages(form: HTMLFormElement): void {
+  form
+    .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+    .forEach((item) => item.classList.remove(SHOW_ERROR_MESSAGES));
+}
+
+function showErrorMessage(input: HTMLInputElement, msg: string): void {
+  const formFields = input.parentElement as HTMLDivElement;
+  const errorMessage = formFields.querySelector(
+    '.error-message',
+  ) as HTMLSpanElement;
+  errorMessage.innerHTML = msg;
+  formFields.classList.add(SHOW_ERROR_MESSAGES);
+}
+
+function shouldSendForm(form: HTMLFormElement): boolean {
+  let send = true;
+  form
+    .querySelectorAll('.' + SHOW_ERROR_MESSAGES)
+    .forEach(() => (send = false));
+  return send;
+}
--- (BBB)-ExerciciosCurso/(022)-Iniciando_TS/webpack.config.js
+++ (BBB)-ExerciciosCurso/(022)-Iniciando_TS/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/A0018-WebPack/index.ts',
+  entry: './src/A0019-Exercicio/A0019-Exercicio.ts',
   module: {
     rules: [
       {
