f666fc6cae8e71f8aa5a3e514ffb69a09cbfb614
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2024-08-19 16:46:38 -0300

    Enviando Práticas do Classes (class) em TS

---
+++ (BBB)-ExerciciosCurso/(022)-Iniciando_TS/src/A0021-Classes/A0021-Classes.ts
@@ -0,0 +1,48 @@
+export class Empresa {
+  // public nome: String = 'Erik';
+  // public readonly nome: String = 'Erik';
+  public readonly nome: String; // public não nescessário
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: String;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+  adicionaColaborador(colaborador: Colaborador): void {
+    this.colaboradores.push(colaborador);
+  }
+
+  mostrarColaborador(): void {
+    for (const colaborador of this.colaboradores) {
+      console.log(colaborador);
+    }
+  }
+}
+
+export class Colaborador {
+  constructor(
+    public readonly nome: String,
+    public readonly sobrenome: String,
+  ) {}
+}
+
+// const empresa1 = new Empresa('Udemy');
+const empresa1 = new Empresa('Udemy', '11.111.111/0001-11');
+const colaborador1 = new Colaborador('Erik', 'Costa');
+const colaborador2 = new Colaborador('Maria', 'Costa');
+const colaborador3 = new Colaborador('João', 'Vieira');
+
+// console.log(empresa1);
+// empresa1.nome = 'Facebook';
+// console.log(empresa1.nome);
+
+empresa1.adicionaColaborador(colaborador1);
+empresa1.adicionaColaborador(colaborador2);
+empresa1.adicionaColaborador(colaborador3);
+empresa1.adicionaColaborador({
+  nome: 'Erik',
+  sobrenome: 'Costa'
+});
+console.log(empresa1);
+empresa1.mostrarColaborador();
