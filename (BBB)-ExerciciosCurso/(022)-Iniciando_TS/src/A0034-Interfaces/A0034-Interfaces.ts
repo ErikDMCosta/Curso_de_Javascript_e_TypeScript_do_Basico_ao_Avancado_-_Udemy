eaaef1bea20eb8c402186d4eaec6010d87622c46
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2024-08-28 20:18:32 -0300

    Enviando Práticas de Interfaces em TypeScript

---
+++ (BBB)-ExerciciosCurso/(022)-Iniciando_TS/src/A0034-Interfaces/A0034-Interfaces.ts
@@ -0,0 +1,39 @@
+interface TipoNome {
+  nome: string;
+}
+
+interface TipoSobrenome {
+  sobrenome: string;
+}
+
+interface TipoNomeCompleto {
+  nomeCompleto(): string;
+}
+
+// type Numero = number | string;
+type TipoPessoa = TipoNome & TipoSobrenome & TipoNomeCompleto;
+interface TipoPessoa2 extends TipoNome, TipoSobrenome, TipoNomeCompleto {}
+
+// export class Pessoa implements TipoNome, TipoSobrenome, TipoNomeCompleto {
+export class Pessoa implements TipoPessoa2 {
+  constructor(
+    public nome: string,
+    public sobrenome: string,
+  ) {}
+
+  nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoaObj: TipoPessoa2 = {
+  nomeCompleto() {
+    return this.nome + ' ' + this.sobrenome;
+  },
+  nome: 'Erik',
+  sobrenome: 'Agora tá ok',
+};
+
+const pessoa = new Pessoa('Erik', 'Costa');
+console.log(pessoa.nomeCompleto());
+console.log(pessoaObj.nomeCompleto());
