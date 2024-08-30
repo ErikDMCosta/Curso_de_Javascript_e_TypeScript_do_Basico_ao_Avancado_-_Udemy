ea4f0d49b0a18bd022f0a5b81e541d5ed7d15ac4
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2024-08-30 20:30:16 -0300

    Enviando Práticas de Interfaces e Classes

---
+++ (BBB)-ExerciciosCurso/(022)-Iniciando_TS/src/A0019-Exercicio/A0019-Exercicio.ts
@@ -0,0 +1,2 @@
+// import './form-control';
+import '../A0035-Exercicio-Video/A0035-Exercicio-Video'
---
+++ (BBB)-ExerciciosCurso/(022)-Iniciando_TS/src/A0035-Exercicio-Video/A0035-Exercicio-Video.ts
@@ -0,0 +1,58 @@
+interface VideoPlayerElements {
+  videoPlayer: HTMLVideoElement;
+  playButton: HTMLButtonElement;
+  stopButton: HTMLButtonElement;
+}
+
+interface VideoPlayerProtocol {
+  playToggle(): void;
+  stop(): void;
+  iniciarEventos(): void;
+}
+
+export default class VideoPlayer implements VideoPlayerProtocol {
+  private videoPlayer: HTMLVideoElement;
+  private playButton: HTMLButtonElement;
+  private stopButton: HTMLButtonElement;
+
+  constructor(videoPlayerElements: VideoPlayerElements) {
+    this.videoPlayer = videoPlayerElements.videoPlayer;
+    this.playButton = videoPlayerElements.playButton;
+    this.stopButton = videoPlayerElements.stopButton;
+  }
+
+  iniciarEventos(): void {
+    this.playButton.addEventListener('click', () => {
+      console.log('OI');
+      this.playToggle();
+    });
+
+    this.stopButton.addEventListener('click', () => {
+      this.videoPlayer.pause();
+      this.videoPlayer.currentTime = 0;
+      this.videoPlayer.innerText = 'Play';
+    });
+  }
+
+  playToggle(): void {
+    if (this.videoPlayer.paused) {
+      this.videoPlayer.play();
+      this.videoPlayer.innerText = 'Pause';
+    } else {
+      this.videoPlayer.pause();
+      this.videoPlayer.innerText = 'Play';
+    }
+  }
+
+  stop(): void {
+    //
+  }
+}
+
+const videoPlayer = new VideoPlayer({
+  videoPlayer: document.querySelector('.video') as HTMLVideoElement,
+  playButton: document.querySelector('.play') as HTMLButtonElement,
+  stopButton: document.querySelector('.stop') as HTMLButtonElement,
+});
+
+videoPlayer.iniciarEventos();
