850755cfacb35360a22f514f52fda6d5f1c45a8d
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2024-09-06 16:48:17 -0300

    Enviando Práticas de Generics com Classes (Estrutura de Dados Pilha)

---
+++ (BBB)-ExerciciosCurso/(022)-Iniciando_TS/src/A0047-Generics-5/A0047-Generics-5.ts
@@ -0,0 +1,53 @@
+export class Pessoa<T, U> {
+  constructor(
+    public nome: T,
+    public idade: U,
+  ) {}
+}
+
+// const array: Array<number>
+export class Pilha<T> {
+  private contador = 0;
+  private elementos: { [k: number]: T } = {};
+
+  push(elemento: T): void {
+    this.elementos[this.contador] = elemento;
+    this.contador++;
+  }
+
+  pop(): T | void {
+    if (this.estaVazia()) return undefined;
+
+    this.contador--;
+    const elemento = this.elementos[this.contador];
+    delete this.elementos[this.contador];
+    return elemento;
+  }
+
+  estaVazia(): boolean {
+    return this.contador === 0;
+  }
+
+  tamanho(): number {
+    return this.contador;
+  }
+
+  mostrarPilha(): void {
+    for (const chave in this.elementos) {
+      console.log(this.elementos[chave]);
+    }
+  }
+}
+
+const pilha = new Pilha();
+
+pilha.push(1);
+pilha.push(2);
+pilha.push(3);
+pilha.push(4);
+// pilha.push('4');
+pilha.mostrarPilha();
+
+while (!pilha.estaVazia()) {
+  console.log(pilha.pop());
+}
