216674468b2fbb2251809a4deb61fcc00a213401
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2024-09-10 16:37:53 -0300

    Enviando Práticas de Utility Types - Generics Padrão no TypeScript

--- (BBB)-ExerciciosCurso/(022)-Iniciando_TS/src/A0049-Generics-7/A0049-Generics-7.ts
+++ (BBB)-ExerciciosCurso/(022)-Iniciando_TS/src/A0049-Generics-7/A0049-Generics-7.ts
@@ -1,3 +1,4 @@
+// -| Type Predicate (Predicado de tipos) |-
 export function isNumber(value: unknown): value is number {
   return typeof value === 'number';
 }
@@ -15,3 +16,73 @@ export function soma(...args: unknown[]): number {
 console.log(soma(1, 2, 3)); // Saída: 6
 console.log(soma(...[1, 2, 3, 'a', 'b', 'c', 1])); // Saída: 7
 console.log(soma('a', 'b', 'c')); // Saída: 0
+
+// -| Utility Types - Generics padrão no TypeScript |-
+// Record
+const objeto1: Record<string, string | number> = {
+  nome: 'Erik',
+  sobrenome: 'Costa',
+  idade: 24,
+};
+console.log(objeto1);
+
+type PessoaProtocol = {
+  nome?: string;
+  sobrenome?: string;
+  idade?: number;
+};
+
+// Required
+type PessoaRequired = Required<PessoaProtocol>;
+// Partial
+type PessoaPartial = Partial<PessoaProtocol>;
+// Readonly
+type PessoaReadonly = Readonly<PessoaProtocol>;
+// Pick
+type PessoaPick = Pick<PessoaProtocol, 'nome' | 'sobrenome'>;
+
+const objeto2: PessoaProtocol = {
+  nome: 'Erik',
+  sobrenome: 'Costa',
+  idade: 24,
+};
+console.log(objeto2);
+
+// Extract e Exclude
+type ABC = 'A' | 'B' | 'C';
+type CDE = 'C' | 'D' | 'E';
+
+// type TipoExclude = ABC;
+type TipoExclude = Exclude<ABC, CDE>;
+// type TipoExtract = ABC;
+type TipoExtract = Extract<ABC, CDE>;
+
+// -|
+type AccountMongo = {
+  _id: string;
+  nome: string;
+  idade: number;
+  sobrenome: string;
+};
+
+type AccountApi = Pick<AccountMongo, Exclude<keyof AccountMongo, '_id'>> & {
+  id: string;
+};
+
+const accountMongo: AccountMongo = {
+  _id: 'asfd9p8a7sdf90a8s76f9as',
+  nome: 'Erik',
+  idade: 24,
+  sobrenome: 'Costa',
+};
+
+function mapAccount(accountMongo: AccountMongo): AccountApi {
+  const { _id, ...accountData } = accountMongo;
+  return { ...accountData, id: _id };
+}
+
+const accountApi = mapAccount(accountMongo);
+console.log(accountApi);
+
+// Module Mode
+export default 1;
