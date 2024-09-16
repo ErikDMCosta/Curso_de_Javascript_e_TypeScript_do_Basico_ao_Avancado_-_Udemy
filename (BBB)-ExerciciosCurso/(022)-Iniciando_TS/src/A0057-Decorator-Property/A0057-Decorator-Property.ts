b7c0e339c641558cf901565de91babab6335396a
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2024-09-16 19:41:29 -0300

    Enviando Práticas de Decoradores de Propriedades (Property Decorators)

---
+++ (BBB)-ExerciciosCurso/(022)-Iniciando_TS/src/A0057-Decorator-Property/A0057-Decorator-Property.ts
@@ -0,0 +1,49 @@
+function decorator(classPrototype: any, nome: string | symbol): any {
+  let valorPropriedade: any;
+  return {
+    get: () => valorPropriedade,
+    set: (valor: any) => {
+      if (typeof valor === 'string') {
+        valorPropriedade = valor.split('').reverse().join('');
+        return;
+      } else {
+        valorPropriedade = valor;
+      }
+    },
+  };
+}
+
+export class UmaPessoa {
+  @decorator
+  nome: string;
+  @decorator
+  sobrenome: string;
+  @decorator
+  idade: number;
+
+  constructor(nome: string, sobrenome: string, idade: number) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.idade = idade;
+  }
+
+  metodo(msg: string): string {
+    return `${this.nome} ${this.sobrenome}: ${msg}`;
+  }
+
+  get nomeCompleto(): string {
+    return this.nome + ' ' + this.sobrenome;
+  }
+
+  set nomeCompleto(valor: string) {
+    const palavras = valor.split(/\s+/g);
+    const primeiroNome = palavras.shift();
+    if (!primeiroNome) return;
+    this.nome = primeiroNome;
+    this.sobrenome = palavras.join(' ');
+  }
+}
+
+const pessoa = new UmaPessoa('Erik', 'Costa', 24);
+const metodo = pessoa.metodo('Olá MUndo!');
+console.log(metodo);
