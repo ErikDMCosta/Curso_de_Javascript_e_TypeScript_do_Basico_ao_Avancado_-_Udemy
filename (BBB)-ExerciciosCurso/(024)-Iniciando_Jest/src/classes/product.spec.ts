36cdd55f89cb94d35569487cd3de1f32092f9dad
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2024-10-29 18:40:27 -0300

    Enviando Práticas de Testando Product.ts

---
+++ (BBB)-ExerciciosCurso/(024)-Iniciando_Jest/src/classes/product.spec.ts
@@ -0,0 +1,19 @@
+import { Product } from './product';
+
+const createSut = (nome: string, price: number): Product => {
+  return new Product(nome, price);
+};
+
+describe('Product', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    // System under test
+    const sut = createSut('Camiseta', 49.9);
+    expect(sut).toHaveProperty('name', 'Camiseta');
+    // expect(sut).toHaveProperty('name', 'Camiseta1');
+    // expect(sut).toHaveProperty('price', 49.9);
+    // expect(sut).toHaveProperty('price', 49.91);
+    expect(sut.price).toBeCloseTo(49.9);
+  });
+});
