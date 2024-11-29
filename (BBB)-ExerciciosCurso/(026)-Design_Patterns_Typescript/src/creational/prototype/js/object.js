54daf97c11152f93679178345dc38cc25958fba1
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2024-11-29 16:43:58 -0300

    Enviando prática do Prototype - Prática

---
+++ (BBB)-ExerciciosCurso/(026)-Design_Patterns_Typescript/src/creational/prototype/js/object.js
@@ -0,0 +1,15 @@
+const personPrototype = {
+  firstName: 'Erik',
+  lastName: 'Costa',
+  age: '24',
+
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+
+const anotherPerson = Object.create(personPrototype);
+anotherPerson.firstName = 'Joana';
+console.log(anotherPerson);
+console.log(anotherPerson.firstName);
+console.log(anotherPerson.fullName());
